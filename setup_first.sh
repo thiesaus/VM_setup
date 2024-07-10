d0e532c65a3b43f02862d56679fa71350fd4fef6
thiesaus <149700688+thiesaus@users.noreply.github.com>
2024-07-10 11:52:55 +0700

    Update setup_first.sh

--- setup_first.sh
+++ setup_first.sh
@@ -2,6 +2,8 @@ pip3 install -r requirements.txt
 sudo apt install python3.10-venv -Y
 sudo python3 -m venv Testing python3.10
 source Testing/bin/activate
+pip3 install wandb -qU
+apt install zip
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 apt install nvidia-cuda-toolkit
 pip3 install kaggle
