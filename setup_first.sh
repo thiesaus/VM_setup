367df758e9201353d4734ed8843d850bfdf72b0e
thiesaus <149700688+thiesaus@users.noreply.github.com>
2024-08-05 08:37:25 +0700

    Update setup_first.sh

--- setup_first.sh
+++ setup_first.sh
@@ -4,6 +4,7 @@ sudo python3 -m venv Testing python3.10
 
 pip3 install -r requirements.txt
 source Testing/bin/activate
+pip install git+https://github.com/openai/CLIP.git
 pip3 install wandb -qU
 apt install zip
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
